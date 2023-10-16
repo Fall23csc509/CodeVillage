07973ed1138f19b23ede14c4269eb17afd1921bd
electropicks <lkbidy@gmail.com>
2023-10-16 15:20:18 -0700

    Implement local data fetching

--- src/test/java/FetchingTests.java
+++ src/test/java/FetchingTests.java
@@ -1,5 +1,6 @@
 import org.codevillage.Utils;
 import org.codevillage.fetching.GithubDataFetcher;
+import org.codevillage.fetching.LocalDataFetcher;
 import org.codevillage.fetching.SVNDataFetcher;
 import org.junit.jupiter.api.Test;
 
@@ -40,5 +41,27 @@ public class FetchingTests {
             e.printStackTrace();
         }
         assert(!tempFolder.exists());
+    }
+    @Test
+    public void testLocalFetching() {
+        final LocalDataFetcher localDataFetcher = new LocalDataFetcher();
+
+        // Provide the source file URL and target directory
+        final String cwd = System.getProperty("user.dir");
+        String sourceFileURL = String.format("file://%s/src/test/resources/local_test_folder", cwd);
+        String targetDirectory = "./.temp";
+
+        localDataFetcher.downloadPackage(sourceFileURL, targetDirectory);
+        // Verify that the files were copied successfully
+        final File tempFolder = new File(targetDirectory);
+        assert (tempFolder.listFiles().length > 0);
+
+        // Delete the temp directory
+        try {
+            Utils.deleteDirectory(tempFolder.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert (!tempFolder.exists());
     }
 }
