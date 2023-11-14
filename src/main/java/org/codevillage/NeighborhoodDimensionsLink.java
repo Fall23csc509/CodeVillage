f7afc48b549c4b24ca782bfec228be85cbab9e56
sam-hud <sam.hud@icloud.com>
2023-11-13 19:30:59 -0800

    feat: add dimensionslink

---
+++ src/main/java/org/codevillage/NeighborhoodDimensionsLink.java
@@ -0,0 +1,10 @@
+package org.codevillage;
+
+public class NeighborhoodDimensionsLink extends ShapeChainLink {
+
+
+    @Override
+    public void position() {
+
+    }
+}
--- src/main/java/org/codevillage/ShapeChainLink.java
+++ src/main/java/org/codevillage/ShapeChainLink.java
@@ -1,7 +1,8 @@
 package org.codevillage;
 
-public abstract class ShapeChainLink implements ShapeChain{
+public abstract class ShapeChainLink implements ShapeChain {
     ShapeChain next;
+
     public ShapeChainLink(ShapeChain next) {
         this.next = next;
     }
