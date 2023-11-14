ba8a19a135f3debd35bc47d36be9c2fce49c2e1c
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2023-11-13 17:52:15 -0800

    Interface Insert Link
    I am going to add tests later when all of our entity classes are actually implemented

---
+++ src/test/java/org/codevillage/InterfaceInsertLinkTest.java
@@ -0,0 +1,12 @@
+package org.codevillage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InterfaceInsertLinkTest {
+    @Test
+    public void InterfaceInsertLinkTest1() {
+        
+    }
+}
