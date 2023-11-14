d54e75cd04785156d2d447538740f9c41e72c52a
sam-hud <sam.hud@icloud.com>
2023-11-13 19:15:49 -0800

    fix: reflect new shape chain changes

--- src/main/java/org/codevillage/PutShapesChainEnd.java
+++ src/main/java/org/codevillage/PutShapesChainEnd.java
@@ -2,12 +2,36 @@ package org.codevillage;
 
 import java.util.List;
 
-public class PutShapesChainEnd extends ShapeChainEnd{
+public class PutShapesChainEnd extends ShapeChainEnd {
+
     @Override
     public void construct() {
-        List<Shape> canvasData = CanvasData.getInstance().getShapes();
-        List<Shape> shapePositioningData = ShapePositioningData.getInstance().getShapes();
-        canvasData.addAll(shapePositioningData);
+
+
+        CanvasData canvasData = CanvasData.getInstance();
+        NeighborhoodWrapper topLevelNeighborhoodWrapper = ShapePositioningData
+                .getInstance()
+                .getNeighborhoodWrapper();
+
+        // Add all shapes in top level wrapper excluding the neighborhood boundary
+        canvasData.addAll(topLevelNeighborhoodWrapper.getShapes());
+
+        // Add each neighborhood and its shapes
+        processNeighborhoods(topLevelNeighborhoodWrapper.getNeighborhoods());
+    }
+
+    private void processNeighborhoods(List<NeighborhoodWrapper> neighborhoodWrapperList) {
+        for (NeighborhoodWrapper neighborhoodWrapper : neighborhoodWrapperList) {
+            // Add neighborhood boundary and its shapes
+            canvasData.add(neighborhoodWrapper.getNeighborhoodShape());
+            canvasData.addAll(neighborhoodWrapper.getShapes());
+
+            // Check if it has neighborhoods remaining
+            if (neighborhoodWrapper.getNeighborhoods()) {
+                // Process the remaining neighborhoods
+                processNeighborhoods(neighborhoodWrapper.getNeighborhoods());
+            }
+        }
     }
 
 }
