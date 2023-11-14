4071fce8ad11a012dfc253a13110f922aaa0c592
sam-hud <sam.hud@icloud.com>
2023-11-14 10:28:23 -0800

    fix: made a start

--- src/main/java/org/codevillage/NeighborhoodDimensionsLink.java
+++ src/main/java/org/codevillage/NeighborhoodDimensionsLink.java
@@ -21,31 +21,41 @@ public class NeighborhoodDimensionsLink extends ShapeChainLink {
         }
     }
 
-    private void processNeighborhoods(List<NeighborhoodWrapper> neighborhoodWrapperList, CanvasData canvasData) {
+    private int processNeighborhoods(List<NeighborhoodWrapper> neighborhoodWrapperList, CanvasData canvasData) {
+        int totalEntityYBoundSize = 10; // Variable to track the total size (start with 10px padding)
+
         for (NeighborhoodWrapper neighborhoodWrapper : neighborhoodWrapperList) {
-
-            // Get to deepest subneighborhood
+            // Continue if more subneighborhoods exist
             if (!neighborhoodWrapper.getNeighborhoods().isEmpty()) {
-                // Go deeper if there are more subneighborhoods
-                processNeighborhoods(neighborhoodWrapper.getNeighborhoods(), canvasData);
+                int subneighborhoodEntityYBoundSize = processNeighborhoods(neighborhoodWrapper.getNeighborhoods(), canvasData) + 10; // Size of all subneighborhoods
+                totalEntityYBoundSize += subneighborhoodEntityYBoundSize; // Keep track of the total size
+            } else {
+                // If no more subneighborhoods exist, use the remaining shapes as the size
+
+                // Create PointShape list from NeighborhoodWrapper shapes
+                List<PointShape> pointShapes = neighborhoodWrapper.getShapes().stream()
+                        .filter(shape -> shape instanceof PointShape)
+                        .map(shape -> (PointShape) shape)
+                        .toList();
+
+                int entityYBoundSize = calculateYHeight(pointShapes) + 10; // + 10px padding
+                totalEntityYBoundSize += entityYBoundSize; // Keep track of the total size
             }
-
-            ArrayList<Integer> sizeY = new ArrayList<>();
-            int size = 10; // padding 10px
-            int shapeSpan = calculateShapeSpan(neighborhoodWrapper.getShapes());
-            int prevEntityYBound =
-            int entityYBound = prevEntityYBound + 10 * (1+  neighborhoodWrapper.getShapes().size()) + shapeSpan;
-            neighborhoodWrapper.setEntityYBound(entityYBound);
-
-
+            neighborhoodWrapper.setEntityYBound(totalEntityYBoundSize);
         }
+        return totalEntityYBoundSize; // Return the total size when done processing (this will be the total size taken up on the canvas)
     }
 
-    private int calculateShapeSpan(List<Shape> shapes) {
-
-        for (Shape shape : shapes) {
-            PointShape pointShape = (PointShape) shape;
-            pointShape.getY();
+    private int calculateYHeight(List<PointShape> shapes) {
+        if (shapes.isEmpty()) {
+            return 0; // When the list is empty
         }
+
+        int maxY = shapes.stream().mapToInt(PointShape::getY).max().orElse(0);
+        int minY = shapes.stream().mapToInt(PointShape::getY).min().orElse(0);
+
+        return maxY - minY;
     }
+
+
 }
---
+++ src/test/java/org/codevillage/NeighborhoodDimensionsLinkTest.java
@@ -0,0 +1,53 @@
+package org.codevillage;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NeighborhoodDimensionsLinkTest {
+    @Test
+    public void neighborhoodDimensionsLinkTest1() {
+        ArrayList<JavaEntity> entities = new ArrayList<>();
+        JavaAbstractClass a = new JavaAbstractClass("A", "A", 0, new ArrayList<>(),
+                new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), null);
+        entities.add(a);
+        ArrayList<String> bCompositions = new ArrayList<>();
+        bCompositions.add("A");
+        JavaAbstractClass b = new JavaAbstractClass("B", "B", 0, new ArrayList<>(),
+                new ArrayList<>(), bCompositions, new ArrayList<>(), null);
+        entities.add(b);
+        JavaBaseClass x = new JavaBaseClass("x", "x", 0, new ArrayList<>(),
+                new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), null);
+        entities.add(x);
+        ArrayList<String> yCompositions = new ArrayList<>();
+        yCompositions.add("A");
+        JavaBaseClass y = new JavaBaseClass("y", "y", 0, new ArrayList<>(),
+                new ArrayList<>(), yCompositions, new ArrayList<>(), null);
+        entities.add(y);
+        ArrayList<String> zCompositions = new ArrayList<>();
+        zCompositions.add("A");
+        zCompositions.add("B");
+        JavaBaseClass z = new JavaBaseClass("z", "z", 0, new ArrayList<>(),
+                new ArrayList<>(), zCompositions, new ArrayList<>(), null);
+        entities.add(z);
+        NeighborhoodWrapper testNeighborhood = new NeighborhoodWrapper();
+        testNeighborhood.addEntity(x);
+        testNeighborhood.addEntity(a);
+        NeighborhoodWrapper n1 = new NeighborhoodWrapper();
+        n1.addEntity(b);
+        n1.addEntity(y);
+        NeighborhoodWrapper n2 = new NeighborhoodWrapper();
+        n2.addEntity(z);
+        n1.addNeighborhood(n2);
+        testNeighborhood.addNeighborhood(n1);
+        ShapePositioningData.getInstance().setNeighborhoodWrapper(testNeighborhood);
+        NeighborhoodDimensionsLink link = new NeighborhoodDimensionsLink(null);
+        link.position(entities);
+        System.out.println(ShapePositioningData.getInstance().getNeighborhoodWrapper().getEntityYBound());
+        System.out.println(ShapePositioningData.getInstance().getNeighborhoodWrapper().getNeighborhoods().toString());
+        assertEquals(0,
+                ShapePositioningData.getInstance().getNeighborhoodWrapper().getEntityYBound()); // Not sure what answer should be
+    }
+}
