86e4ba2ffdf33aef881be132330262da7b5bbaa0
khoa-l <kly27@calpoly.edu>
2023-11-15 13:45:09 -0800

    fix: refactory NeighborhoodFactory and RoadFactory to implement ShapeFactory

--- src/main/java/org/codevillage/NeighborhoodFactory.java
+++ src/main/java/org/codevillage/NeighborhoodFactory.java
@@ -1,16 +1,13 @@
 package org.codevillage;
 
-public class NeighborhoodFactory
-{
-    public static Neighborhood createNeighborhood()
-    {
-        return new Neighborhood();
-    }
+public class NeighborhoodFactory implements ShapeFactory {
+  public Shape createShape() {
+    return new Neighborhood();
+  }
 
-    public static Neighborhood fromTopLeftCorner(int left, int top, int width, int height)
-    {
-        int centerX = left + width / 2;
-        int centerY = top + height / 2;
-        return new Neighborhood(centerX, centerY, width, height);
-    }
+  public static Neighborhood fromTopLeftCorner(int left, int top, int width, int height) {
+    int centerX = left + width / 2;
+    int centerY = top + height / 2;
+    return new Neighborhood(centerX, centerY, width, height);
+  }
 }
