702bde11428f9e4e933154ae40c3801b8b52b2b4
skylerdee3 <73thieny@gmail.com>
2023-11-15 02:56:51 -0800

    added entities for parser

---
+++ src/main/java/org/codevillage/JavaEntityFactory.java
@@ -0,0 +1,11 @@
+package org.codevillage;
+
+import java.io.File;
+
+public class JavaEntityFactory {
+
+    public JavaEntity createEntityFromFile(File file) {
+        return null;
+    }
+
+}
--- src/main/java/org/codevillage/SourceCodeParser.java
+++ src/main/java/org/codevillage/SourceCodeParser.java
@@ -1,14 +1,20 @@
 package org.codevillage;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class SourceCodeParser implements Parser{
+    ArrayList<JavaEntity> entities;
 
     @Override
     public void parseSourceFiles(File directory) {
         SourceFileChecker fileChecker = new SourceFileChecker();
+        JavaEntityFactory entityFactory = new JavaEntityFactory();
         // validate java files as source code
         File[] sourceCodeFiles = fileChecker.getFilesToParse(directory);
+        for (File file : sourceCodeFiles){
+            entities.add(entityFactory.createEntityFromFile(file));
+        }
     }
     
 }
