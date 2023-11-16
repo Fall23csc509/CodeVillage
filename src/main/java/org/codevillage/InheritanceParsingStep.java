a0383f9ec060dcfbabbfd7d860feb154b2041b22
DhillonTaran <132304214+DhillonTaran@users.noreply.github.com>
2023-11-15 16:21:37 -0800

    Update InheritanceParsingStep.java

--- src/main/java/org/codevillage/InheritanceParsingStep.java
+++ src/main/java/org/codevillage/InheritanceParsingStep.java
@@ -1,10 +1,12 @@
 package org.codevillage;
 
+import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import java.util.Set;
-import java.util.HashSet;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class InheritanceParsingStep extends EntityParsingStep {
 
@@ -14,74 +16,40 @@ public class InheritanceParsingStep extends EntityParsingStep {
 
     @Override
     public JavaEntity construct(EntityBuilder builder, CompilationUnit declaration) {
-        // Existing logic to parse the entity
-        JavaEntity entity = next.construct(builder, declaration);
+        InheritanceVisitor visitor = new InheritanceVisitor();
+        visitor.visit(declaration, builder);
 
-        // New logic to get associated class names
-        Set<String> associatedClassNames = getAssociatedClassNames(entity);
-        associatedClassNames.forEach(builder::addAssociation);
-
-        return entity;
+        if (this.next != null) {
+            return this.next.construct(builder, declaration);
+        } else {
+            return builder.build();
+        }
     }
 
-    private Set<String> getAssociatedClassNames(Object entity) {
-        Set<String> classNames = new HashSet<>();
-        Class<?> entityClass = entity.getClass();
-
-        // Inspect superclass
-        Class<?> superClass = entityClass.getSuperclass();
-        if (superClass != null && superClass != Object.class) {
-            classNames.add(superClass.getName());
-        }
-
-        // Inspect interfaces
-        Class<?>[] interfaces = entityClass.getInterfaces();
-        for (Class<?> interfaceClass : interfaces) {
-            classNames.add(interfaceClass.getName());
-        }
-
-        // Inspect fields
-        Field[] fields = entityClass.getDeclaredFields();
-        for (Field field : fields) {
-            Class<?> fieldType = field.getType();
-            classNames.add(fieldType.getName());
-        }
-
-        // Inspect methods
-        Method[] methods = entityClass.getDeclaredMethods();
-        for (Method method : methods) {
-            Class<?> returnType = method.getReturnType();
-            classNames.add(returnType.getName());
-            Class<?>[] parameterTypes = method.getParameterTypes();
-            for (Class<?> parameterType : parameterTypes) {
-                classNames.add(parameterType.getName());
+    private static class InheritanceVisitor extends VoidVisitorAdapter<EntityBuilder> {
+        
+        @Override
+        public void visit(ClassOrInterfaceDeclaration n, EntityBuilder builder) {
+            super.visit(n, builder);
+            // Check if it's a class and has a superclass
+            if (n.isClassOrInterfaceDeclaration() && n.getExtendedTypes().isNonEmpty()) {
+                String parentClass = n.getExtendedTypes().get(0).getNameAsString();
+                builder.parent(parentClass);
             }
         }
-
-        return classNames;
     }
 
-    public static void main(String[] args) {
-        // Mock setup for demonstration
-        JavaEntityBuilder builder = new JavaEntityBuilder();
-        builder.name("MockEntity");
-        builder.fullyQualifiedName("org.codevillage.MockEntity");
-        builder.type(JavaEntityType.JAVA_BASE_CLASS);
+    public static void main(String[] args) throws IOException {
+        String sourceCode = new String(Files.readAllBytes(Paths.get("path/to/YourJavaFile.java")));
+        CompilationUnit compilationUnit = StaticJavaParser.parse(sourceCode);
 
-        // Mock entity class (normally, you would parse a real Java file to get a CompilationUnit)
-        class MockEntity extends Object {
-            // Mock fields and methods
-        }
-
-        // Setting up the parsing chain
-        EntityParsingChain terminalStep = new EntityParsingFinish(); // Terminal step in the chain
+        EntityParsingChain terminalStep = new BuildParsingFinish(); 
         EntityParsingChain inheritanceStep = new InheritanceParsingStep(terminalStep);
+        JavaEntityBuilder builder = new JavaEntityBuilder();
 
-        // Constructing the entity using the parsing chain
-        JavaEntity entity = inheritanceStep.construct(builder, null); // Passing null for CompilationUnit for simplicity
-
-        // Output the associated class names
-        System.out.println("Associated Class Names for " + entity.getName() + ":");
-        entity.getAssociations().forEach(System.out::println);
+        JavaEntity entity = inheritanceStep.construct(builder, compilationUnit);
+        
+        // Output the parent class name
+        System.out.println("Parent Class for " + entity.getName() + ": " + entity.getParent().orElse("No Parent"));
     }
 }
